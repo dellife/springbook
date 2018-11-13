77ad69a89bec23e163d5988a799718a254309601
sehee <sehee@gwonsehuiui-MacBook-Pro.local>
2018-11-13 22:01:21 +0900

    develop 2

--- src/main/java/com/dellife/springbook/SpringbookApplication.java
+++ src/main/java/com/dellife/springbook/SpringbookApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbookApplication {
 	// developing...
+	// developing...2
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbookApplication.class, args);
